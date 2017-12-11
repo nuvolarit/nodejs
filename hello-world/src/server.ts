e2e6f417ac902ba810f2bd5c0deac6406a522339
nuvolarit <you@example.com>
2017-12-11 21:27:22 +0100

    added express lodash

--- hello-world/src/server.ts
+++ hello-world/src/server.ts
@@ -1,11 +1,19 @@
-import * as http from "http";
+//import * as http from "http";
+//import * as lodash from "lodash";
+
+import { createServer } from "http";
+import { each } from "lodash";
 
 const portNumber = 3000;
 
-http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World!')
-    console.log(`handled request ${req.url}`)
-}).listen(portNumber);
+each([1, 2, 3, 4], (i) => {
+    console.log(i);
+});
 
-console.log(`server listening on port ${portNumber}...`);
+// createServer((req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     res.end('Hello World!')
+//     console.log(`handled request ${req.url}`)
+// }).listen(portNumber);
+
+// console.log(`server listening on port ${portNumber}...`);
